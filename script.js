b4b020bca0c1a95b441d51034dc043a00dd71f30
kai-kai20 <gomezkaira11@gmail.com>
2024-11-22 21:08:24 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarADN() {
+  const descripcion = document.getElementById('adn-description');
+  descripcion.innerHTML = "Aquí se mostraría una visualización interactiva de la doble hélice del ADN, utilizando gráficos o animaciones.";
+}
